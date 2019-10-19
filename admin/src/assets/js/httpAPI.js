3c81d1666d4e11965d0ba90b1d5220ad577227b3
dlf <1055205416@qq.com>
2019-10-19 23:42:26 +0800

    aster

--- admin/src/assets/js/httpAPI.js
+++ admin/src/assets/js/httpAPI.js
@@ -1,25 +1,44 @@
 import axios from 'axios'
 
+axios.defaults.timeout = 10000;
 
-export function get(url, params){    
-    return new Promise((resolve, reject) =>{        
-        axios.get(url, {            
-            params: params        
-        }).then(res => {
-            resolve(res.data);
-        }).catch(err =>{
-            reject(err.data)        
-    })    
-});}
+// Add a request interceptor
+axios.interceptors.request.use(function (config) {
+  // Do something before request is sent
+  return config;
+}, function (error) {
+  // Do something with request error
+  return Promise.reject(error);
+});
+// Add a response interceptor
+axios.interceptors.response.use(function (response) {
+  // Do something with response data
+  return response;
+}, function (error) {
+  // Do something with response error
+  return Promise.reject(error);
+});
+
+export function get(url, params) {
+  return new Promise((resolve, reject) => {
+    axios.get(url, {
+      params: params
+    }).then(res => {
+      resolve(res.data);
+    }).catch(err => {
+      reject(err.data)
+    })
+  });
+}
 
 export function post(url, params) {
-    return new Promise((resolve, reject) => {
-         axios.post(url, params)
-        .then(res => {
-            resolve(res.data);
-        })
-        .catch(err =>{
-            reject(err.data)
-        })
-    });
+  return new Promise((resolve, reject) => {
+    axios.post(url, params)
+      .then(res => {
+        resolve(res.data);
+      })
+      .catch(err => {
+        reject(err.data)
+      })
+  });
 }
--- admin/src/main.js
+++ admin/src/main.js
@@ -6,18 +6,45 @@ import router from './router'
 
 import elementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import {get,post} from './assets/js/httpAPI'
+import {
+  get,
+  post
+} from './assets/js/httpAPI'
 import httpUrl from './assets/js/httpUrl'
 
 Vue.config.productionTip = false
-Vue.prototype.$get=get;
-Vue.prototype.$post=post;
-Vue.prototype.url=httpUrl;
+Vue.prototype.$get = get;
+Vue.prototype.$post = post;
+Vue.prototype.url = httpUrl;
 Vue.use(elementUI)
 /* eslint-disable no-new */
+
+// const whiteList=['/login'];//不需要登录能访问的path
+// router.beforeEach((to, from, next) => {
+//   console.log('beforeEach');
+//   var userInfo= JSON.parse(sessionStorage.getItem('userInfoStorage'));//获取缓存看是否登录过
+//   if(whiteList.indexOf(to.path)<0){//访问了需要登录才能访问的页面
+//     if(userInfo){//登录过来直接进去
+//       next();
+//     }else{
+//       if(to.path=='/login'){
+//         next();
+//       }else{
+//         next('/login');
+//       }
+//     }
+//   }
+//   else{
+//     next();
+//   }
+// });
+
+
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>'
 })
